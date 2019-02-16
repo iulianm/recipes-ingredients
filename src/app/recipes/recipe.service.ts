6ed8e3139a156a047eb7ee562a94a1decce6e80f
Iulian <iulian.moisa@gmail.com>
2019-02-16 14:14:48 -0800

    services and dependency injection

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,34 @@
+import { Recipe } from "./recipe.model";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      "Tasty Schnitzel",
+      "A super-tasty Schnitzel - just awesome!",
+      "https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
+    ),
+    new Recipe(
+      "Big Fat Burger",
+      "What else you need to say?",
+      "https://i.kinja-img.com/gawker-media/image/upload/s--l6rS3nZj--/c_scale,f_auto,fl_progressive,q_80,w_800/vcwkkj2ayw6eez1rbwdt.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 2)]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
